8744ff72419ca5c88bdd767d49fccfddefb527ba
Matthias Brettschneider <frosch03@gmail.com>
2012-08-10 13:57:24 +0200

    deleted some comments from the arrow-class

--- Control.Arrow.hs
+++ Control.Arrow.hs
@@ -23,16 +23,6 @@ class (Category a) => Arrow a where
     arr    :: (VNat n) 
            => (Vec n b -> Vec n b) -> a (Vec n b) (Vec n b)
 
--- first and second are not any more useable
--- better would be something like heads and tails 
--- with an additionally VNat parameter defining how many of them
-
---  first  :: (VNat n, VNat j, VAdd n j nj) 
---         => a (Vec n b) (Vec n b) -> a (Vec nj b) (Vec nj b)
-
---  second :: (VNat n, VNat j, VAdd j n jn) 
---         => a (Vec j b) (Vec j b) -> a (Vec jn b) (Vec jn b)
-
     firsts :: (VNat n, VNat nj, VLs n nj VTrue)
            => n -> a (Vec n b) (Vec n b) -> a (Vec nj b) (Vec nj b)
 
